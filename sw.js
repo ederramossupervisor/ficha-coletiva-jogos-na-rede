6f0dc99504a897fde09108f1285072fa1567b679
ederramossupervisor <eder.ramos@educador.edu.es.gov.br>
2026-04-24 14:15:44 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,24 +6,19 @@ const urlsToCache = [
   './manifest.json'
 ];
 
-// Instala e armazena os recursos essenciais
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Estratégia: Cache First, depois rede (para uso offline do formulário)
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(cachedResponse => {
       if (cachedResponse) {
         return cachedResponse;
       }
-      return fetch(event.request).then(response => {
-        // Não armazene respostas da Cloud Function ou Apps Script, pois são dinâmicas e falham offline mesmo assim
-        return response;
-      });
+      return fetch(event.request);
     })
   );
 });
